40ade7a2ec8216c5bb277f0dacfeadb05ae68e59
ddsyasas <ddsyasas@gmail.com>
2026-01-14 09:05:45 +0100

    Add Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -302,6 +302,24 @@ export default function RootLayout({
           <Toaster />
           <Sonner />
         </TooltipProvider>
+        {/* Tawk.to Live Chat */}
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69674a02a64fd41980797379/1jetni6b4';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
